45a6dd786845b7b055d81bdc1e7d233153e05023
Markus Nörenberg <markus.noerenberg@fondofbags.com>
2018-08-24 13:31:12 +0200

    storefinder

--- storefinder.js
+++ storefinder.js
@@ -1,40 +1,267 @@
 'use strict';
 
-var $ = require('jquery');
+const $ = require('jquery');
+const MarkerClusterer = require('node-js-marker-clusterer');
 
-window.jQuery = window.jQuery || $;
+/**
+ * @param {Array} storefinderCustomOptions
+ * @param {String} apiKey
+ */
+function init(storefinderCustomOptions, apiKey)
+{
+    toggleContactAddressInfoboxOnDetailPage();
 
-var StoreFinder = $.StoreFinder = {};
-var self = {},
-    markerCluster = null;
+    let apiUrl = '//maps.googleapis.com/maps/api/js?v=3&key=' + apiKey;
+    $.getScript(apiUrl, function()
+    {
+        let storefinderOptions = getStorefinderOptions(storefinderCustomOptions);
+        let mapOptions = getMapOptions(storefinderOptions);
 
-// Validation.add('required-country', 'Please select a country.', function(v) {
-//     if ($('#storefinder-filter-form-input-address').val()
-//         && ( $('#storefinder-filter-form-input-country').val() == 'all'  || $('#storefinder-filter-form-input-country').val() == '' )
-//     ){
-//         return false;
-//     }
-//
-//     return true;
-// });
+        let map = initializeMap(storefinderOptions, mapOptions);
 
-// init plugin
-$.StoreFinder.init = function (options) {
+        let markerClusterOptions = getMarkerClusterOptions(storefinderOptions);
+        let markerCluster = initializeMarkerCluster(map, markerClusterOptions);
 
-    self = $.extend({
-        //google maps settings
-        scrollwheel: false,
+        initializeMarkers(map, markerCluster, storefinderOptions);
+    });
+}
+
+/**
+ * @returns void
+ */
+function toggleContactAddressInfoboxOnDetailPage()
+{
+    $('.address-contact-buttons > a').on('click', function(e) {
+        var value = $(this).data('value');
+
+        if (value) {
+            e.preventDefault();
+            $('.address-contact-items .item').each(function(index, item) {
+                let itemJquery = $(item);
+
+                if (itemJquery.hasClass(value)){
+                    if (!itemJquery.hasClass('active')) {
+                        itemJquery.addClass('active');
+                    } else {
+                        itemJquery.removeClass('active');
+                    }
+                } else if(itemJquery.hasClass('active')) {
+                    itemJquery.removeClass('active');
+                }
+            });
+        } else {
+            $('.address-contact-items .item.active').removeClass('active');
+        }
+    });
+}
+
+/**
+ * @param {Object} map
+ * @param {Object} markerCluster
+ * @param {String} storefinderOptions
+ */
+function initializeMarkers(map, markerCluster, storefinderOptions)
+{
+    $.getJSON(getCurrentSearchQuery(storefinderOptions), function(data)
+    {
+        let markers = createMarkers(map, data, storefinderOptions);
+
+        markerCluster.clearMarkers();
+        markerCluster.addMarkers(markers, false);
+    });
+}
+
+/**
+ * @param {Object} map
+ * @param {String} storefinderOptions
+ */
+function setMapZoomLevel(map, storefinderOptions)
+{
+    let zoomChangeBoundsListener = google.maps.event.addListener(map, 'bounds_changed', function () {
+        google.maps.event.removeListener(zoomChangeBoundsListener);
+        map.setZoom(storefinderOptions.zoomLevel);
+    });
+}
+
+/**
+ * @param {Object} map
+ * @param {Array} data
+ * @param {String} storefinderOptions
+ * @returns {Array}
+ */
+function createMarkers(map, data, storefinderOptions)
+{
+    let markers = [];
+    let bounds = new google.maps.LatLngBounds();
+    let infoWindows = [];
+
+    $.each(data, function(index)
+    {
+        let item = this;
+        let latitude = item.latitude;
+        let longitude = item.longitude;
+        let popoverContent = item.content;
+
+        if (latitude === '0' || longitude === '0') {
+            return true;
+        }
+
+        let markerLatLng = new google.maps.LatLng(latitude, longitude);
+        bounds.extend(markerLatLng);
+
+        let icon = new google.maps.MarkerImage(storefinderOptions.markerMapsIconUrl, null, null, null, null);
+        let marker = new google.maps.Marker({
+            position: markerLatLng,
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: icon
+        });
+
+        markers.push(marker);
+
+        // create marker popup
+        if (storefinderOptions.showInfoWindow) {
+            infoWindows[index] = new google.maps.InfoWindow({
+                content: popoverContent
+            });
+
+            // add event listener to marker
+            google.maps.event.addListener(marker, 'click', function () {
+                closeInfoWindows(infoWindows);
+                infoWindows[index].open(map, this);
+            });
+        }
+
+        map.fitBounds(bounds);
+        map.panToBounds(bounds);
+    });
+
+    return markers;
+}
+
+/**
+ * @param {Array} infoWindows
+ */
+function closeInfoWindows(infoWindows) {
+    for (var i = 0; i < infoWindows.length; i++) {
+        infoWindows[i].close();
+    }
+}
+
+/**
+ * @param {Object} map
+ * @param {Array} markerClusterOptions
+ */
+function initializeMarkerCluster(map, markerClusterOptions)
+{
+    return new MarkerClusterer(map, [], markerClusterOptions);
+}
+
+/**
+ * @param {Array} storefinderOptions
+ * @returns {Array}
+ */
+function getMarkerClusterOptions(storefinderOptions)
+{
+    return {
+        styles: [
+            {
+                width: 52,
+                height: 53,
+                textColor: storefinderOptions.markerTextColor,
+                url: storefinderOptions.markerIconUrl
+            }
+        ],
+        maxZoom: 12,
+        gridSize: 20
+    }
+}
+
+/**
+ * @param {String} storefinderOptions
+ * @returns {Array}
+ */
+function getCurrentSearchQuery(storefinderOptions)
+{
+    let searchQuery = storefinderOptions.urlSearch;
+
+    let params = (new URL(document.location)).searchParams;
+    if (params.has('country')) {
+        searchQuery += '?country=' + params.get('country');
+    }
+
+    if (params.has('address')) {
+        searchQuery += '&address=' + params.get('address');
+    }
+
+    return searchQuery;
+}
+
+/**
+ * @param {Array} storefinderCustomOptions
+ * @returns {Array}
+ */
+function getStorefinderOptions(storefinderCustomOptions)
+{
+    let storefinderDefaultOptions = getStorefinderDefaultOptions();
+    let storefinderOptions = $.extend(storefinderDefaultOptions, storefinderCustomOptions || {});
+
+    let storefinderMapContainer = getStorefinderMapContainer(storefinderOptions);
+
+    return $.extend(storefinderOptions, getStorefinderDataOptions(storefinderMapContainer) || {});
+}
+
+/**
+ * @param {Object} storefinderMapContainer
+ * @returns {Array}
+ */
+function getStorefinderDataOptions(storefinderMapContainer)
+{
+    let storefinderMapContainerDataOptions = {};
+
+    let latitude = storefinderMapContainer.data('latitude');
+    let longitude = storefinderMapContainer.data('longitude');
+    let zoomLevel = storefinderMapContainer.data('zoomlevel');
+
+    if (latitude) {
+        storefinderMapContainerDataOptions.latitude = latitude;
+    }
+
+    if (longitude) {
+        storefinderMapContainerDataOptions.longitude = longitude;
+    }
+
+    if (zoomLevel) {
+        storefinderMapContainerDataOptions.zoomLevel = zoomLevel;
+    }
+
+    return storefinderMapContainerDataOptions;
+}
+
+/**
+ * @param {Array} storefinderOptions
+ * @returns {Object}
+ */
+function getStorefinderMapContainer(storefinderOptions)
+{
+    return $('#' + storefinderOptions.mapCanvas);
+}
+
+/**
+ * @returns {Array}
+ */
+function getStorefinderDefaultOptions()
+{
+    return {
+        scrollWheel: false,
         latitude: 52.51966798345114,
         longitude: 13.40306282043457,
         mapCanvas: 'storefinder-map',
         zoomLevel: 3,
         zoomControl: true,
         disableDefaultUI: true,
-        searchButton: $('.storefinder-form button'),
-        searchResultBlock: $('.storefinder .search-list'),
-        radiusSelect: $('#storefinder-radius'),
         searchForm: $('#storefinder-filter-form'),
-        urlSearch: window.location.origin + '/de/' + 'storefinder/ajax/search/',
+        urlSearch: window.location.origin + '/de/storefinder/search',
         markers: '',
         markerIcon: '',
         showInfoWindow : true,
@@ -42,283 +269,43 @@ $.StoreFinder.init = function (options) {
         maxZoom: 16,
         layout: '',
         markerTextColor: '#ffffff',
-
-        onInit: function () {
-
-            var mapCanvas = $('#' + self.mapCanvas),
-                latitude = mapCanvas.data('latitude'),
-                longitude = mapCanvas.data('longitude'),
-                zoomLevel = mapCanvas.data('zoomlevel'),
-                showInfoWindow = mapCanvas.data('showinfowindow');
-
-            if (latitude){
-                self.latitude = latitude;
-            }
-
-            if (longitude){
-                self.longitude = longitude;
-            }
-
-            if (zoomLevel){
-                self.zoomLevel = zoomLevel;
-            }
-
-            if (typeof showInfoWindow !== 'undefined'){
-                self.showInfoWindow = showInfoWindow;
-            }
-
-            var map = initGM();
-            var markers = getMarkers();
-
-            setMarker(map, markers);
-
-        }
-
-
-    }, options || {});
-
-    self.onInit();
-};
+        markerIconUrl: window.location.origin + '/assets/affenzahn/images/m1.png',
+        markerMapsIconUrl: window.location.origin + '/assets/affenzahn/images/marker.png',
+    }
+}
 
 /**
- * init google map
- *
- * @param mapsJson
- * @param $
+ * @param {Array} storefinderOptions
+ * @returns {Array}
  */
-var initGM = function (data) {
-
-    var currentRadius = self.radiusSelect.val();
-    switch (currentRadius) {
-        case '10' :
-            self.zoomLevel = 14;
-            break;
-        case '20' :
-            self.zoomLevel = 12;
-            break;
-        case '50' :
-            self.zoomLevel = 8;
-            break;
-        case '100' :
-            self.zoomLevel = 7;
-            break;
-        case '200' :
-            self.zoomLevel = 6;
-            break;
-        default:
-            self.zoomLevel = self.zoomLevel;
-    }
-    var options = {
-        styles: self.layout,
+function getMapOptions(storefinderOptions)
+{
+    return {
+        styles: storefinderOptions.layout,
         scrollwheel: false,
-        zoom: self.zoomLevel,
-        scrollWheel: self.scrollWheel,
-        zoomControl: self.zoomControl,
-        mapTypeControl: self.mapTypeControl,
-        disableDefaultUI: self.disableDefaultUI,
-        center: new google.maps.LatLng(self.latitude, self.longitude),
+        zoom: storefinderOptions.zoomLevel,
+        scrollWheel: storefinderOptions.scrollWheel,
+        zoomControl: storefinderOptions.zoomControl,
+        mapTypeControl: storefinderOptions.mapTypeControl,
+        disableDefaultUI: storefinderOptions.disableDefaultUI,
+        center: new google.maps.LatLng(storefinderOptions.latitude, storefinderOptions.longitude),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        minZoom : self.minZoom,
-        maxZoom : self.maxZoom
+        minZoom : storefinderOptions.minZoom,
+        maxZoom : storefinderOptions.maxZoom
     };
-
-    // build map
-    var map = new google.maps.Map(document.getElementById(self.mapCanvas), options);
-    setMarker(map, data);
-
-    return map;
-};
-
+}
 
 /**
- * Retrieve Markers
- */
-var getMarkers = function () {
-
-    var parameters = [];
-
-    if (self.searchForm !== null) {
-        parameters = self.searchForm.serialize();
-    }
-
-    var mapCanvas = $('#' + self.mapCanvas);
-    self.urlSearch = mapCanvas.data('ajaxurl');
-
-    if (mapCanvas.data('storeid')){
-        if (parameters){
-            parameters = '&id=' + mapCanvas.data('storeid');
-        }else{
-            parameters = 'id=' + mapCanvas.data('storeid');
-        }
-    }
-
-    // send post action on submit
-    $.post(self.urlSearch, parameters, function (response) {
-        if (response.state === 'success'){
-            initGM(response.data);
-        }
-
-    }, 'json');
-};
-
-var setMarker = function(map, data){
-    var bounds = new google.maps.LatLngBounds(),
-        infoWindow = [],
-        mapCanvas = $('#' + self.mapCanvas);
-
-    if (mapCanvas.data('zoomlevel')){
-        self.zoomLevel = mapCanvas.data('zoomlevel');
-    }
-
-    var zoomChangeBoundsListener =
-        google.maps.event.addListener(map, 'bounds_changed', function () {
-            google.maps.event.removeListener(zoomChangeBoundsListener);
-            map.setZoom(self.zoomLevel);
-        });
-
-
-    var markerIconsUrl = self.markerIcon = mapCanvas.data('markericonurl'),
-        mcOptions = {
-            styles: [
-                {
-                    width: 52,
-                    height: 53,
-                    textColor: self.markerTextColor,
-                    url: markerIconsUrl + '/m1.png'
-                }
-            ],
-            maxZoom: 12,
-            gridSize: 20
-        },
-        clusterMarkers = [];
-
-    // set marker for each JSON item
-    if (data){
-        $.each(data.items, function (index) {
-            if (this.longitude !== '0' && this.latitude !== '0'){
-                var markerLatLng = new google.maps.LatLng(this.latitude, this.longitude);
-                bounds.extend(markerLatLng);
-
-                var icon = new google.maps.MarkerImage(markerIconsUrl + '/marker.png', null, null, null, null);
-
-                var marker = new google.maps.Marker({
-                    position: markerLatLng,
-                    map: map,
-                    animation: google.maps.Animation.DROP,
-                    icon: icon
-                });
-
-                clusterMarkers.push(marker);
-
-                infoWindow[index] = new google.maps.InfoWindow({
-                    content: this.content
-                });
-
-                // add event listener to marker
-                if (self.showInfoWindow){
-                    google.maps.event.addListener(marker, 'click', function () {
-                        closeInfoWindows(infoWindow);
-                        infoWindow[index].open(map, this);
-                    });
-                }
-
-                map.fitBounds(bounds);
-                map.panToBounds(bounds);
-
-            }
-        });
-    }
-
-    var markerCluster = new MarkerClusterer(map, clusterMarkers, mcOptions);
-};
-
-/**
- * close info windows
+ * @param {Array} storefinderOptions
+ * @param {Array} googleMapsOptions
  *
- * @param infoWindow
+ * @return google.maps.Map
  */
-var closeInfoWindows = function (infoWindow) {
-    for (var i = 0; i < infoWindow.length; i++) {
-        infoWindow[i].close();
-    }
-};
+function initializeMap(storefinderOptions, googleMapsOptions)
+{
+    let container = document.getElementById(storefinderOptions.mapCanvas);
 
-var init = function (options) {
-
-    $(document).ready(function () {
-
-        StoreFinder.init(options);
-
-
-        if( $('#storefinder-map').length ){
-            var form = document.getElementById("storefinder-filter-form");
-
-            if (form){
-                var dataForm = new VarienForm('storefinder-filter-form', true);
-                form.addEventListener("submit", function (e) {
-                    e.preventDefault();
-
-                    if(dataForm.validator && dataForm.validator.validate()){
-                        var actionUrl = $(this).attr('action'),
-                            urlParams = getUrlParams();
-
-                        if (urlParams.length){
-                            actionUrl = actionUrl + '?' + urlParams.join('&');
-                        }
-
-                        $(this).attr('action', actionUrl);
-                        $(this).submit();
-
-                    }
-                });
-            }
-        }
-
-        $('.address-contact-buttons > a').on( "click", function(e) {
-            var value = $(this).data('value');
-
-            if (value){
-                e.preventDefault();
-                $('.address-contact-items .item').each(function(index, item){
-                    if ($(item).hasClass(value)){
-                        if (!$(item).hasClass('active')){
-                            $(item).addClass('active');
-                        }else{
-                            $(item).removeClass('active');
-                        }
-                    }else if($(item).hasClass('active')) {
-                        $(item).removeClass('active');
-                    }
-                });
-
-            }else{
-                $('.address-contact-items .item.active').removeClass('active');
-            }
-        });
-
-    });
-
-    /**
-     * Retrieve Url Params
-     *
-     * @return string
-     */
-    function getUrlParams(){
-        var urlParams = new Array();
-
-        if ($('#storefinder-filter-form-input-country').val()){
-            var country = $('#storefinder-filter-form-input-country').val();
-            urlParams.push('country=' + encodeURIComponent(country));
-        }
-
-        if ($('#storefinder-filter-form-input-address').val()){
-            var address = $('#storefinder-filter-form-input-address').val();
-            urlParams.push('address=' + encodeURIComponent(address));
-        }
-
-        return urlParams;
-    }
-
-};
+    return new google.maps.Map(container, googleMapsOptions);
+}
 
 exports.init = init;
